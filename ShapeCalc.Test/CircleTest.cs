804925f17aee944f0092698a4652d45656e14bbe
User <gold.like.simple@gmail.com>
2023-03-22 17:35:16 +1000

    [+] Add Unit test for Triangle and Circle

---
+++ ShapeCalc.Test/CircleTest.cs
@@ -0,0 +1,41 @@
+﻿using ShapeCalc.Database.Models;
+using ShapeCalc.Services.Services;
+using Xunit;
+
+namespace ShapeCalc.Test
+{
+    public class CircleServiceTests
+    {
+        private readonly ICircleService _circleService;
+
+
+        public CircleServiceTests()
+        {
+            _circleService = new CircleService();
+        }
+
+
+        [Fact]
+        public async Task GetArea_ValidCircle_ReturnsCorrectResult()
+        {
+            var circle = new Circle {Radius = 5};
+            const double expectedArea = Math.PI * 5 * 5;
+
+            var actualArea = await _circleService.GetArea(circle);
+
+            Assert.Equal(expectedArea, actualArea);
+        }
+
+
+        [Fact]
+        public async Task GetArea_InvalidCircle_Return_0_If_InvalidArea()
+        {
+            var circle = new Circle {Radius = -1};
+            const double expectedValue = 0;
+
+            var actualArea = await _circleService.GetArea(circle);
+
+            Assert.Equal(expectedValue, actualArea);
+        }
+    }
+}
---
+++ ShapeCalc.Test/TriangleTests.cs
@@ -0,0 +1,58 @@
+﻿using ShapeCalc.Database.Models;
+using ShapeCalc.Services.Services;
+using Xunit;
+
+namespace ShapeCalc.Test
+{
+    public class TriangleTest
+    {
+        private readonly ITriangleService _triangleService;
+
+        public TriangleTest()
+        {
+            _triangleService = new TriangleService();
+        }
+
+        [Fact]
+        public async Task GetArea_ValidTriangle_ReturnsCorrectArea()
+        {
+            var triangle = new Triangle { SideA = 3, SideB = 4, SideC = 5 };
+            const double expectedArea = 6.0;
+            
+            var actualArea = await _triangleService.GetArea(triangle);
+            
+            Assert.Equal(expectedArea, actualArea);
+        }
+        
+        [Fact]
+        public async Task GetArea_InvalidTriangle_Return_0_If_InvalidArea()
+        {
+            var badTriangle = new Triangle { SideA = 1, SideB = 3, SideC = 50 };
+            var actualArea = await _triangleService.GetArea(badTriangle);
+            
+            const int expectedValue = 0;
+            Assert.Equal(expectedValue, actualArea);
+        }
+
+
+        [Fact]
+        public async Task IsRightAngle_ValidTriangle_ReturnsTrue()
+        {
+            var triangle = new Triangle { SideA = 3, SideB = 4, SideC = 5 };
+            
+            var isRightAngle = await _triangleService.IsRightAngle(triangle);
+            
+            Assert.True(isRightAngle);
+        }
+
+        [Fact]
+        public async Task IsRightAngle_InvalidTriangle_ReturnsFalse()
+        {
+            var triangle = new Triangle { SideA = 1, SideB = 2, SideC = 5 };
+            
+            var isRightAngle = await _triangleService.IsRightAngle(triangle);
+            
+            Assert.False(isRightAngle);
+        }
+    }
+}
